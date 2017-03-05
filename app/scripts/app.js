b2687ef5238530c775f61866c924551108712b0d
Julia Eng <julia.eng55@gmail.com>
2017-03-04 21:14:01 -0500

    change callback

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,7 @@ angular
     'wu.masonry'
   ]).config(function(SpotifyProvider) {
     SpotifyProvider.setClientId('854e4a39ed614f8db67aa9d424093981');
-    SpotifyProvider.setRedirectUri('http://localhost:8080/6/Artify/app/callback.html');
+    SpotifyProvider.setRedirectUri('https://julia-eng.github.io/Artify/app/callback.html');
     SpotifyProvider.setScope('user-read-private playlist-read-private');
     // // If you already have an auth token
     // SpotifyProvider.setAuthToken('zoasliu1248sdfuiknuha7882iu4rnuwehifskmkiuwhjg23');
