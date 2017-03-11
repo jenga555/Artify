f0f70faa962d6a5e3e4240fb4f96c48af4bd70f3
Julia Eng <julia.eng55@gmail.com>
2017-03-11 02:41:45 -0500

    Change redirect

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,8 +6,8 @@ angular
     'wu.masonry'
   ]).config(function(SpotifyProvider) {
     SpotifyProvider.setClientId('854e4a39ed614f8db67aa9d424093981');
-    // SpotifyProvider.setRedirectUri('https://julia-eng.github.io/Artify/app/callback.html');
-    SpotifyProvider.setRedirectUri('http://localhost:8080/6/Artify/app/callback.html');
+    SpotifyProvider.setRedirectUri('https://julia-eng.github.io/Artify/app/callback.html');
+    // SpotifyProvider.setRedirectUri('http://localhost:8080/6/Artify/app/callback.html');
     // SpotifyProvider.setRedirectUri('http://dev6.jhuep.com/~jeng9/6/callback.html');
     SpotifyProvider.setScope('user-read-private playlist-read-private playlist-read-collaborative');
 
